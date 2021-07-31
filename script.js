dc4c3bfba8fa7b04a40a00f5638edb48db28411c
avyukd <avyukd@gmail.com>
2021-07-31 13:09:07 -0400

    Working barebones NavSearch extension.

---
+++ script.js
@@ -0,0 +1,29 @@
+const postSelection = (info, tab) => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+
+        fetch(`http://127.0.0.1:8000/test/save`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'selectedText': info.selectionText,
+                'url': url,
+                'origin': "Chrome"
+            })
+        }).then(
+            response => {
+                alert("Saved!");
+            }
+        )
+
+    });
+    
+}
+
+chrome.contextMenus.create({
+    title: "Save on NavSearch", 
+    contexts:["selection"], 
+    onclick: postSelection
+});
